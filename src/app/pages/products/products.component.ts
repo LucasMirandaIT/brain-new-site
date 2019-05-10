7ab23bf34df128274c7ed35a4554e790dca62588
LucasMirandaIT <miranda.lucas@hotmail.com>
2019-05-10 09:43:44 -0300

    new fix for products variable

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -15,13 +15,8 @@ export class ProductsComponent implements OnInit {
   produtos;
   mobile: boolean;
 
-  slides = [
-    {img: "https://cdn2.outdoorphotographer.com/2019/01/Moody_Landscapes_1.jpg"},
-    {img: "https://cdn.fstoppers.com/styles/large-16-9/s3/lead/2018/06/ultra-wide-mistakes-lead-image.jpg"},
-    {img: "https://static.photocdn.pt/images/articles/2017/12/11/articles/2017_8/iStock-875430954-min.jpg"},
-    {img: "https://cdn.theatlantic.com/assets/media/img/photo/2017/09/landscapes-of-norway/n03_844052000/main_900.jpg?1507146797"}
-  ];
-  slideConfig = {"slidesToShow": 1,  "arrows": true, "dots": true};
+
+  slideConfig = {"slidesToShow": 1, "arrows": true, "dots": true};
 
   constructor(private http: Http) { }
 
